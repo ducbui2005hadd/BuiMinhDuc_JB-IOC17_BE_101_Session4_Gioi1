f4b0f07ec8367418343f0b32f1a82f3aeb0de122
Bùi Minh Đức <160729391+ducbui2005hadd@users.noreply.github.com>
2025-11-07 23:27:28 +0700

    Update Gioi1

---
+++ Gioi1.sql
@@ -0,0 +1,52 @@
+DROP TABLE IF EXISTS students;
+
+CREATE TABLE students (
+    id INT PRIMARY KEY,
+    full_name VARCHAR(100),
+    gender VARCHAR(10),
+    birth_year INT,
+    major VARCHAR(50),
+    gpa DECIMAL(3, 1) 
+);
+
+INSERT INTO students (id, full_name, gender, birth_year, major, gpa) VALUES
+(1, 'Nguyễn Văn A', 'Nam', 2002, 'CNTT', 3.6),
+(2, 'Trần Thị Bích Ngọc', 'Nữ', 2001, 'Kinh tế', 3.2),
+(3, 'Lê Quốc Cường', 'Nam', 2003, 'CNTT', 2.7),
+(4, 'Phạm Minh Anh', 'Nữ', 2000, 'Luật', 3.9),
+(5, 'Nguyễn Văn A', 'Nam', 2002, 'CNTT', 3.6),
+(6, 'Lưu Đức Tài', 'Nam', 2004, 'Cơ khí', NULL), 
+(7, 'Võ Thị Thu Hằng', 'Nữ', 2001, 'CNTT', 3.0);
+
+INSERT INTO students (id, full_name, gender, birth_year, major, gpa)
+VALUES (8, 'Phạm Hoàng Nam', 'Nam', 2003, 'CNTT', 3.8);
+
+UPDATE students
+SET gpa = 3.4
+WHERE full_name = 'Lê Quốc Cường';
+
+DELETE FROM students
+WHERE gpa IS NULL;
+
+SELECT *
+FROM students
+WHERE major = 'CNTT' AND gpa >= 3.0
+LIMIT 3;
+
+SELECT DISTINCT major
+FROM students;
+
+SELECT *
+FROM students
+WHERE major = 'CNTT'
+ORDER BY 
+    gpa DESC,      
+    full_name ASC;  
+
+SELECT *
+FROM students
+WHERE full_name LIKE 'Nguyễn%';
+
+SELECT *
+FROM students
+WHERE birth_year BETWEEN 2001 AND 2003;
